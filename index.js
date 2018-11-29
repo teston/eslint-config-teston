7d839c82f2d693f45b2b5df898850491a85adb4e
Ridge Frederick <ridge.frederick@gmail.com>
2018-11-28 18:25:59 -0600

    Setup common configuration for ESLint in use across projects
    Use babel-preset-teston
    
    
    Reorganize deps
    
    
    Update .gitignore

---
+++ index.js
@@ -0,0 +1,27 @@
+module.exports = {
+  plugins: ['vue'],
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module',
+  },
+  extends: ['airbnb-base', 'plugin:vue/essential', 'prettier', 'prettier/vue'],
+  rules: {
+    'no-mixed-operators': 0,
+    'max-len': 0,
+    radix: 0,
+    'no-param-reassign': [2, { props: false }],
+    // allow i++ in for loops
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+    // don't require .vue extension when importing
+    'import/extensions': [
+      'error',
+      'always',
+      {
+        js: 'never',
+        vue: 'never',
+      },
+    ],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+  },
+};
